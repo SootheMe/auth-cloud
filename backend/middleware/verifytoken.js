26e81f83f5e7faa51be7a7ec6ffe9ac25c0ea85e
aauullliiaa <72453696+aauullliiaa@users.noreply.github.com>
2023-06-10 02:03:58 +0800

    Update verifytoken.js

--- backend/middleware/verifytoken.js
+++ backend/middleware/verifytoken.js
@@ -4,9 +4,9 @@ export const verifyToken = (req, res, next) => {
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
     if(token == null) return res.sendStatus(401);
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
         if(err) return res.sendStatus(403);
-        req.email = decoded.email;
+        req.user = user;
         next();
     })
 }
