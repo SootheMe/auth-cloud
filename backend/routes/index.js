f9e212028af81f323dc6c9ed8ea388c8fb5e054a
aauullliiaa <72453696+aauullliiaa@users.noreply.github.com>
2023-06-10 02:05:23 +0800

    add GET method for /username endpoint

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { Register, getUsers, Login, Logout } from "../controllers/users.js";
+import { Register, getUsers, Login, Logout, getUserName } from "../controllers/users.js";
 import { verifyToken } from "../middleware/verifytoken.js";
 import { refreshToken } from "../controllers/refreshtoken.js";
 
@@ -8,6 +8,7 @@ const router = express.Router();
 router.get('/users', verifyToken, getUsers);
 router.post('/users', Register);
 router.post('/login', Login);
+router.get('/username', verifyToken, getUserName);
 router.get('/token', refreshToken);
 router.delete('/logout', Logout)
 
